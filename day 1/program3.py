74c07017b792475fcb3cae45224b5ae40a5b1a55
rohitramdurge <rohitramdurge@gmail.com>
2025-12-13 08:45:37 +0530

    day 1

---
+++ day 1/program3.py
@@ -0,0 +1,8 @@
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+
+print("Before swap: a =", a, ", b =", b)
+
+a, b = b, a   # swapping
+
+print("After swap: a =", a, ", b =", b)
---
+++ day 1/program4.py
@@ -0,0 +1,7 @@
+x = float(input("Enter first number: "))
+y = float(input("Enter second number: "))
+
+print("Addition:", x + y)
+print("Subtraction:", x - y)
+print("Multiplication:", x * y)
+print("Division:", x / y)
